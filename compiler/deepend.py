b4083453327b276de791af1635d1aacd6c2e2720
Tatsiana Mikhalevich <82167820+dusia-tm@users.noreply.github.com>
2025-08-14 16:11:38 +0300

    Update deepend.py

--- compiler/deepend.py
+++ compiler/deepend.py
@@ -1,1 +1,20 @@
+замены = {
+    "функция": "def",
+    "вернуть": "return",
+    "вывести": "print",
+    "если": "if",
+    "иначе": "else",
+}
 
+def transpile(code):
+    for ru, en in замены.items():
+        code = code.replace(ru, en)
+    return code
+
+# Пример использования:
+deep_end_code = """
+функция сложить(a, b):
+    вернуть a + b
+"""
+python_code = transpile(deep_end_code)
+print(python_code)
